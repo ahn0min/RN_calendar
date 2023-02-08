85e95172bca49ce1b76217be1183be5615711f4b
YeongMin Ahn <ahn0min95@gmail.com>
2023-02-08 23:31:22 +0900

    Add: Cheveron Icon Component

--- src/components/common/Icons/index.tsx
+++ src/components/common/Icons/index.tsx
@@ -2,6 +2,7 @@ import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
 
 export type GetIconFunction = (color: string) => JSX.Element;
+export type IconFunctionProps = {size: number; color: string};
 
 const Icons = {
   Home: (color: string) => (
@@ -16,6 +17,12 @@ const Icons = {
   User: (color: string) => (
     <FontAwesomeIcon name="user" size={30} color={color} />
   ),
-} as {[key: string]: GetIconFunction};
+  ChevronLeft: ({size, color}: IconFunctionProps) => (
+    <FontAwesomeIcon name="chevron-left" size={size} color={color} />
+  ),
+  ChevronRight: ({size, color}: IconFunctionProps) => (
+    <FontAwesomeIcon name="chevron-right" size={size} color={color} />
+  ),
+} as const;
 
 export default Icons;
