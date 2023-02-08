a4d45d98d44378214f082c46ef83843e2e0cf8a4
YeongMin Ahn <ahn0min95@gmail.com>
2023-02-09 02:51:58 +0900

    Chore: Date 객체의 각종 데이터를 추출하는 중복 코드 제거

--- src/hooks/useCalander.ts
+++ src/hooks/useCalander.ts
@@ -1,8 +1,8 @@
 import {useState} from 'react';
-
+import {transformDateIntoNumberData} from '../utils/transformDateIntoNumberData';
 interface IUseCalenaderProps {
   newDate: Date;
-  location: 'en-US' | 'kr';
+  location: 'en-US' | 'ko-KR' | undefined;
   toLocaleDateStringOptions: Intl.DateTimeFormatOptions;
 }
 
@@ -11,32 +11,28 @@ export const useCalander = ({
   location,
   toLocaleDateStringOptions,
 }: IUseCalenaderProps) => {
-  // 이 date자체를 기반으로 새로운 date를 만들어주자.
   const [selectedDate, setSelectedDate] = useState<Date>(newDate);
 
-  // 각 연도와 월, 일 state 가 바뀌면 저절로 바뀌지 않을까?
-  // TODO: dho getMonth + 1을 해주면 setState에서 - 2 혹은 그대로를 해야할까?
-  const fullYear = selectedDate.getFullYear();
-  const monthWithNumber = selectedDate.getMonth() + 1;
-  const monthWithEng = selectedDate.toLocaleDateString(
+  const {fullYear, month, date, day} =
+    transformDateIntoNumberData(selectedDate);
+
+  const monthWithLocale = selectedDate.toLocaleDateString(
     location,
     toLocaleDateStringOptions,
   );
-  const date = selectedDate.getDate();
-  const day = selectedDate.getDay();
 
   const changePrevMonth = () => {
-    setSelectedDate(new Date(fullYear, monthWithNumber - 2));
+    setSelectedDate(new Date(fullYear, month - 2));
   };
   const changeNextMonth = () => {
-    setSelectedDate(new Date(fullYear, monthWithNumber));
+    setSelectedDate(new Date(fullYear, month));
   };
 
   return {
     selectedDate,
     fullYear,
-    monthWithNumber,
-    monthWithEng,
+    monthWithNumber: month,
+    monthWithLocale,
     date,
     day,
     changePrevMonth,
